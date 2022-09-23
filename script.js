b569f9e6557d9a705955d5cf238055d75d749bf6
lachero <asaf@utopia.tech>
2022-09-23 15:19:27 +0300

    completed

--- script.js
+++ script.js
@@ -1,35 +1,55 @@
 const formEl = document.querySelector('form');
 const inputEl = document.querySelector('#query');
+const feedbackEl = document.querySelector('.feedback');
+
+function copyToClipboard(text) {
+	navigator.clipboard.writeText(text);
+}
 
 function sleep(ms = 1) {
 	return new Promise(resolve => setTimeout(resolve, ms));
 }
 
 function getResults() {
-	for (let i = 0; i < 10000; i++) {
-		console.log('something' + i);
-	}
-	return ['cook', 'cook'];
-	const containerEl = document.querySelector('#rso');
+	const searchContainer = document.querySelector('#search');
+	const items = searchContainer.querySelectorAll('.MjjYud');
 
-	const allEls = containerEl.querySelectorAll('div link[rel=prerender]');
-	console.log(allEls);
-	console.log(containerEl);
+	const urls = [];
+	items.forEach(element => {
+		const a = element.querySelector('.yuRUbf a');
+		if (!a) return;
+		urls.push(a.href);
+	});
+
+	return urls;
 }
 
+let submitted = false;
 async function handleFormSubmit(e) {
+	if (submitted || !inputEl.value) return;
+	submitted = true;
+	feedbackEl.textContent = '';
 	e.preventDefault();
+	try {
+		const tab = await chrome.tabs.create({
+			url: `https://www.google.com/search?q=${inputEl.value ?? ''}`,
+			active: false,
+		});
 
-	const tab = await chrome.tabs.create({
-		url: `https://www.google.com/search?q=${inputEl.value ?? ''}`,
-	});
+		await sleep(1000);
 
-	await sleep();
-
-	const data = await chrome.scripting.executeScript({
-		target: { tabId: tab.id },
-		func: getResults,
-	});
-	console.log(data[0].result);
+		const data = await chrome.scripting.executeScript({
+			target: { tabId: tab.id },
+			func: getResults,
+		});
+		await chrome.tabs.remove(tab.id);
+		copyToClipboard(data[0].result.join('\n'));
+		feedbackEl.textContent = 'copied successfully';
+		inputEl.value = '';
+	} catch (err) {
+		console.log(err);
+		feedbackEl.textContent = 'something went wrong';
+	}
+	submitted = false;
 }
 formEl.addEventListener('submit', handleFormSubmit);
